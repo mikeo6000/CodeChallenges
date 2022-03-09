9b0ff6c13db81a3e6bcf088e67609ee11559ffb9
MIKE MIKE <mikeo6000@hotmail.com>
2022-03-09 13:30:10 -0500

    CodeChallengeMarch7

---
+++ CodeChallengeMarch7/Program.cs
@@ -0,0 +1,19 @@
+﻿
+long [] sum = { 0, 1,0};
+long sumofEven = 0;
+
+for (int i = 0; sum[2] < 4000000; i++)
+{
+    sum[2] = sum[0]+ sum[1];
+    Console.WriteLine(sum[2]);
+    if (sum[2] % 2 == 0)
+    {
+        sumofEven = sumofEven + sum[2];
+    }
+    sum[0] = sum[1];
+    sum[1] = sum[2];
+}
+Console.WriteLine("Sum of even number is "+sumofEven);
+
+
+
